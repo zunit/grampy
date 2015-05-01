2c27681d485a458bbdc756b884de1baacbc46255
zunit <jackjiananzhou@gmail.com>
2015-05-01 13:54:40 -0400

    connected with local postgre

--- app/models.py
+++ app/models.py
@@ -5,20 +5,33 @@ class user_post(db.Model):
     author = db.Column(db.String(64))
     post = db.Column(db.Text())
 
+    def __init__(self, author, post):
+        self.author = author
+        self.post = post
+
     def __repr__(self):
         return '<User %r>' % (self.author)
 
 class photo(db.Model):
-	photo_id = db.Column(db.Integer, primary_key=True)
-	photo_url=db.Column(db.Text)
+    photo_id = db.Column(db.Integer, primary_key=True)
+    photo_url = db.Column(db.Text())
 
-	def __repr__(self):
-		return '<User %r>' % (self.photo_url)
+    def __init__(self, photo_url):
+        self.photo_url = photo_url
+        
+    def __repr__(self):
+    	return '<Photo %r>' % (self.photo_url)
 
 class chinese_user_post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.String(64))
     post = db.Column(db.Text())
 
+    def __init__(self, author, post):
+        self.author = author
+        self.post = post
+
     def __repr__(self):
         return '<User %r>' % (self.author)
+
+db.create_all()
--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://localhost/grampy'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+print(SQLALCHEMY_DATABASE_URI)
+SECRET_KEY = '5E3Lkip847$H$PawMf489tVEa#eCU67KIJ}438mpG[nWaOV<u8g:ydC9RMs'
 
 # pagination
 POSTS_PER_PAGE = 3
