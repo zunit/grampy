d7b60642c87160c41e39c5f5eab212877bdcf07f
zunit <jackjiananzhou@gmail.com>
2015-05-05 10:12:42 -0400

    print line for sql alchemy path

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+print(SQLALCHEMY_DATABASE_URI)
 #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://localhost/grampy'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
