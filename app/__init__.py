179c0a078a6e9680dcfb04992add46bc7f216194
zunit <jackjiananzhou@gmail.com>
2015-05-08 10:43:20 -0400

    revert to previous state

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,8 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask_s3 import FlaskS3
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-app.config['S3_BUCKET_NAME'] = 'grampy101'
-s3 = FlaskS3(app)
 
 from app import views, models
