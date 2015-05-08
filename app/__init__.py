8ce9de8f9478e020049c1c72d3c22ec5fb8b6847
zunit <jackjiananzhou@gmail.com>
2015-05-08 10:29:43 -0400

    connecting s3

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-
-
+app.config['S3_BUCKET_NAME'] = 'grampy101'
+s3 = FlaskS3(app)
 
 from app import views, models
